75e939fcf7b62f63364e4653bca3161b75ab3e78
quokka <devonsaliga@gmail.com>
2025-01-19 21:22:03 -0500

    scraping working. spotify api creds loaded

--- Python46ScrapeBillboard/main.py
+++ Python46ScrapeBillboard/main.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
+from dotenv import load_dotenv
+import os
+
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+
+
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
